eb073bfad6a6cce6771705a3dcf31f9e3abab633
José Lucas <jose.esbellute@gmail.com>
2023-06-03 11:28:40 -0300

    initial commit

---
+++ comanda-backend.Domain/Models/Products/ProductsModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace comanda_backend.Domain.Models.Products
+{
+	public class ProductsModel  
+	{
+		[JsonPropertyName("id")]
+		public Guid Id { get; set; }
+
+		[JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("value")]
+        public decimal Value { get; set; }
+
+        [JsonPropertyName("quantity")]
+        public short Quantity { get; set; }
+    }
+}
+
